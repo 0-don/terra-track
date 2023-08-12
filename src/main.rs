cca5f8989f351570588a293c4fbace11beea4bb5
Don-Cryptus <don.cryptus@gmail.com>
2023-08-12 20:32:16 +0200

    easy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::process::Command;
+use std::str;
 fn main() {
+    let mut command = Command::new("nmap");
+    let output = command.output().unwrap();
+
+    // stdout to utf8
+    println!(
+        "{:?}",
+        str::from_utf8(output.stdout.as_slice()).unwrap()
+    );
+
     println!("Hello, world!");
 }
