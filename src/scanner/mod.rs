24eaef1d7c0d2242ad268d5515a04d8100e93fb8
don.cryptus <don.cryptus@gmail.com>
2023-10-22 22:19:31 +0200

    easy

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -1,4 +1,5 @@
-use futures::{stream::FuturesUnordered, Future, StreamExt};
+use futures::Future;
+use futures::{stream::FuturesUnordered, StreamExt};
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use std::{
@@ -25,43 +26,47 @@ pub struct Scanner {
 impl Scanner {
     pub fn new(ip: IpAddr) -> Self {
         Self {
+            ip,
             batch_size: 4500,
             timeout: Duration::from_millis(1000),
             tries: 1,
-            ip,
         }
     }
 
+    // Main scanning function
     pub async fn run(&self) -> Vec<u16> {
         let mut ports: Vec<u16> = (LOWEST_PORT_NUMBER..=TOP_PORT_NUMBER).collect();
         ports.shuffle(&mut thread_rng());
 
-        let mut open_ports: Vec<u16> = Vec::new();
-        let mut ftrs = FuturesUnordered::new();
-        let mut errors: HashSet<String> = HashSet::with_capacity(65535);
+        let mut open_ports = Vec::new();
+        let mut futures = FuturesUnordered::new();
+        let mut errors = HashSet::with_capacity(TOP_PORT_NUMBER as usize);
 
         for &port in &ports {
             let socket = SocketAddr::new(self.ip, port);
-            ftrs.push(self.scan_socket(socket));
+            futures.push(self.scan_socket(socket));
 
-            // Check if we've reached the batch size
-            if ftrs.len() == self.batch_size as usize {
-                Self::process_future(&mut ftrs, &mut open_ports, &mut errors).await;
+            if futures.len() == self.batch_size as usize {
+                self.process_futures(&mut futures, &mut open_ports, &mut errors)
+                    .await;
             }
         }
 
-        // Process remaining futures if any
-        Self::process_future(&mut ftrs, &mut open_ports, &mut errors).await;
+        // Process any remaining futures
+        self.process_futures(&mut futures, &mut open_ports, &mut errors)
+            .await;
 
         open_ports
     }
 
-    pub async fn process_future(
-        ftrs: &mut FuturesUnordered<impl Future<Output = Result<SocketAddr, Error>>>,
+    // Process a batch of futures
+    async fn process_futures(
+        &self,
+        futures: &mut FuturesUnordered<impl Future<Output = Result<SocketAddr, Error>>>,
         open_ports: &mut Vec<u16>,
         errors: &mut HashSet<String>,
     ) {
-        while let Some(result) = ftrs.next().await {
+        while let Some(result) = futures.next().await {
             match result {
                 Ok(socket) => open_ports.push(socket.port()),
                 Err(e) => {
@@ -71,34 +76,25 @@ impl Scanner {
         }
     }
 
+    // Scan a single socket
     async fn scan_socket(&self, socket: SocketAddr) -> io::Result<SocketAddr> {
-        let tries = self.tries;
-
-        for nr_try in 1..=tries {
-            match self.connect(socket).await {
-                Ok(mut x) => {
-                    if let Err(e) = x.shutdown().await {
-                        println!("Shutdown stream error {}", &e);
-                    }
-                    println!("Open {}", socket.to_string());
-                    return Ok(socket);
+        for _ in 1..=self.tries {
+            if let Ok(mut stream) = self.connect(socket).await {
+                println!("Open {}", socket.to_string());
+                if let Err(e) = stream.shutdown().await {
+                    println!("Shutdown stream error {}", e);
                 }
-                Err(e) => {
-                    let mut error_string = e.to_string();
-
-                    if nr_try == tries {
-                        error_string.push(' ');
-                        error_string.push_str(&socket.ip().to_string());
-                        return Err(io::Error::new(io::ErrorKind::Other, error_string));
-                    }
-                }
-            };
+                return Ok(socket);
+            }
         }
-        unreachable!();
+        Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!("Failed to connect to {}", socket.ip()),
+        ))
     }
 
+    // Attempt to connect to a socket with a timeout
     async fn connect(&self, socket: SocketAddr) -> io::Result<TcpStream> {
-        let stream = timeout(self.timeout, TcpStream::connect(socket)).await??;
-        Ok(stream)
+        timeout(self.timeout, TcpStream::connect(socket)).await?
     }
 }
