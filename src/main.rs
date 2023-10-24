2d942c1a78bc15eaa2ccdd4e03a13db06777c8c6
don.cryptus <don.cryptus@gmail.com>
2023-10-24 22:53:40 +0200

    easy

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ mod ip_iterator;
 mod scanner;
 mod scripts;
 mod utils;
-use db::connect_db;
 use dotenvy::dotenv;
 use ip_iterator::Ipv4Iter;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv().expect(".env file not found");
-    connect_db().await?;
+    // connect_db().await?;
 
     let cursor = "0.0.0.0"; // Start from this IP
     let mut ip_iter = Ipv4Iter::new(cursor);
@@ -18,7 +17,7 @@ async fn main() -> anyhow::Result<()> {
     printlog!("Starting scan...");
 
     while let Some(ip) = ip_iter.next() {
-        // println!("{}", ip);
+        println!("{}", ip);
     }
 
     printlog!("Scan complete");
