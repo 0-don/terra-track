d811a37a134312834e30af9937ae91deb622c13c
don.cryptus <don.cryptus@gmail.com>
2023-10-28 01:40:01 +0200

    easy

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -25,6 +25,26 @@ pub struct Migrator;
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m_000001_scan_batch::Migration)]
+        vec![
+            Box::new(m_000001_scan_batch::Migration),
+            Box::new(m_000002_ip_main::Migration),
+            Box::new(m_000003_ip_privacy::Migration),
+            Box::new(m_000004_ip_location::Migration),
+            Box::new(m_000005_ip_connection::Migration),
+            Box::new(m_000006_ip_network_details::Migration),
+            Box::new(m_000007_ip_security_flags::Migration),
+            Box::new(m_000008_ip_organization::Migration),
+            Box::new(m_000009_ip_contact_details::Migration),
+            Box::new(m_000010_ip_abuse_contact::Migration),
+            Box::new(m_000011_ip_flag::Migration),
+            Box::new(m_000012_ip_hosting_details::Migration),
+            Box::new(m_000013_ip_metadata::Migration),
+            Box::new(m_000014_ip_address::Migration),
+            Box::new(m_000015_ip_port::Migration),
+            Box::new(m_000016_ip_port_state::Migration),
+            Box::new(m_000017_ip_service::Migration),
+            Box::new(m_000018_ip_stats::Migration),
+            Box::new(m_000019_ip_times::Migration),
+        ]
     }
 }
