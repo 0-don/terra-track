5335d9babf1214729ccf13e9ce3dbbc7aefe3174
don.cryptus <don.cryptus@gmail.com>
2023-11-25 02:47:47 +0100

    easy

--- service/src/entity/ip_host_script/ip_host_script_m.rs
+++ service/src/entity/ip_host_script/ip_host_script_m.rs
@@ -1,13 +1,13 @@
 use crate::db::get_db_connection;
-use ::entity::ip_service;
-use sea_orm::{ActiveModelTrait, Set, TryIntoModel};
+use ::entity::ip_host_script;
+use sea_orm::{ActiveModelTrait, EntityTrait, Set, TryIntoModel};
 
 pub struct Mutation;
 
 impl Mutation {
     pub async fn create_ip_host_script(
-        active_model: ip_service::ActiveModel,
-    ) -> anyhow::Result<ip_service::Model> {
+        active_model: ip_host_script::ActiveModel,
+    ) -> anyhow::Result<ip_host_script::Model> {
         let db = get_db_connection().await?;
         let model = active_model.save(&db).await?.try_into_model()?;
 
@@ -16,10 +16,10 @@ impl Mutation {
 
     pub async fn update_ip_host_script(
         id: i64,
-        model: ip_service::Model,
-    ) -> anyhow::Result<ip_service::Model> {
+        model: ip_host_script::Model,
+    ) -> anyhow::Result<ip_host_script::Model> {
         let db = get_db_connection().await?;
-        let model = ip_service::ActiveModel {
+        let model = ip_host_script::ActiveModel {
             id: Set(id),
             ..model.into()
         }
@@ -32,12 +32,23 @@ impl Mutation {
 
     pub async fn delete_ip_host_script(id: i64) -> anyhow::Result<bool> {
         let db = get_db_connection().await?;
-        ip_service::ActiveModel {
+        ip_host_script::ActiveModel {
             id: Set(id),
             ..Default::default()
         }
         .delete(&db)
         .await?;
+
+        Ok(true)
+    }
+
+    pub async fn create_many_ip_host_script(
+        active_models: Vec<ip_host_script::ActiveModel>,
+    ) -> anyhow::Result<bool> {
+        let db = get_db_connection().await?;
+        ip_host_script::Entity::insert_many(active_models)
+            .exec(&db)
+            .await?;
 
         Ok(true)
     }
--- service/src/entity/ip_host_script/ip_host_script_q.rs
+++ service/src/entity/ip_host_script/ip_host_script_q.rs
@@ -1,28 +1,26 @@
 use crate::db::get_db_connection;
-use ::entity::ip_service;
+use ::entity::ip_host_script;
 use sea_orm::{prelude::DateTimeWithTimeZone, ColumnTrait, EntityTrait, QueryFilter, QueryTrait};
 
 pub struct Query;
 
 impl Query {
-    pub async fn find_ip_os_by_id(id: i64) -> anyhow::Result<Option<ip_service::Model>> {
+    pub async fn find_host_script_by_id(id: i64) -> anyhow::Result<Option<ip_host_script::Model>> {
         let db = get_db_connection().await?;
-        let model = ip_service::Entity::find_by_id(id).one(&db).await?;
+        let model = ip_host_script::Entity::find_by_id(id).one(&db).await?;
 
         Ok(model)
     }
 
-    pub async fn find_ip_os_by_ip_main_id_older_then(
-        port: i16,
+    pub async fn find_host_script_by_ip_main_id_older_then(
         ip_main_id: i64,
         time_ago: Option<DateTimeWithTimeZone>,
-    ) -> anyhow::Result<Option<ip_service::Model>> {
+    ) -> anyhow::Result<Option<ip_host_script::Model>> {
         let db = get_db_connection().await?;
-        let model = ip_service::Entity::find()
-            .filter(ip_service::Column::Port.eq(port))
-            .filter(ip_service::Column::IpMainId.eq(ip_main_id))
+        let model = ip_host_script::Entity::find()
+            .filter(ip_host_script::Column::IpMainId.eq(ip_main_id))
             .apply_if(time_ago, |query, date| {
-                query.filter(ip_service::Column::CreatedAt.lt(date))
+                query.filter(ip_host_script::Column::CreatedAt.lt(date))
             })
             .one(&db)
             .await?;
@@ -30,10 +28,10 @@ impl Query {
         Ok(model)
     }
 
-    // pub async fn find_ip_service_by_ip(ip: &String) -> anyhow::Result<Option<ip_service::Model>> {
+    // pub async fn find_ip_host_script_by_ip(ip: &String) -> anyhow::Result<Option<ip_host_script::Model>> {
     //     let db = get_db_connection().await?;
-    //     let model = ip_service::Entity::find()
-    //         .filter(ip_service::Column::IpAddress.contains(ip))
+    //     let model = ip_host_script::Entity::find()
+    //         .filter(ip_host_script::Column::IpAddress.contains(ip))
     //         .one(&db)
     //         .await?;
 
