76de2121f39836e765740ed7c9c04bb66fb4a911
don.cryptus <don.cryptus@gmail.com>
2023-11-25 02:50:39 +0100

    easy

--- service/src/parser.rs
+++ service/src/parser.rs
@@ -1,3 +1,4 @@
+use crate::entity::ip_host_script::ip_host_script_m;
 use crate::entity::ip_main::ip_main_m;
 use crate::entity::ip_service::ip_service_m;
 use crate::entity::ip_service_script::ip_service_script_m;
@@ -15,20 +16,25 @@ pub async fn parse_nmap_results(nmap: &Nmap) -> anyhow::Result<()> {
     let ip = &host.address.addr;
     let ports = &host.ports.port;
 
+    // MAIN IP
     let ip_main = ip_main_m::Mutation::upsert_ip_main_by_ip(ip, &host.address.addrtype).await?;
 
+    // SERVICES
     let mut services_to_create = Vec::new();
     for port in ports {
         services_to_create.push(process_service(ip_main.id, &port));
     }
-
     let created_services =
         ip_service_m::Mutation::create_many_ip_services(services_to_create).await?;
 
+    // HOST SCRIPTS
     if let Some(host_script) = &host.hostscript {
-        ip_host_script_mapper::process_host_script(ip_main.id, host_script).await?;
+        let host_scripts =
+            ip_host_script_mapper::process_host_script(ip_main.id, host_script).await?;
+        ip_host_script_m::Mutation::create_many_ip_host_script(host_scripts).await?;
     }
 
+    // PORT SCRIPTS
     let mut script_models = Vec::new();
     for (created_service, port) in created_services.iter().zip(ports.iter()) {
         if let Some(script) = &port.script {
@@ -39,7 +45,6 @@ pub async fn parse_nmap_results(nmap: &Nmap) -> anyhow::Result<()> {
             ));
         }
     }
-
     ip_service_script_m::Mutation::create_many_ip_service_scripts(script_models).await?;
 
     printlog!("Parsing nmap results End");
