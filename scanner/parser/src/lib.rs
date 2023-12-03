9aec0a07ebf3a72ad8503fe3d29fa55bd53fd9b0
don.cryptus@gmail.com <don.cryptus@gmail.com>
2023-12-03 20:58:46 +0100

    easy

---
+++ scanner/parser/src/lib.rs
@@ -0,0 +1,8 @@
+pub mod ip_iterator;
+pub mod nmap_scanner;
+pub mod port_scanner;
+pub mod types;
+
+pub static VALUE: &str = "value";
+pub static ELEM: &str = "elem";
+pub static TABLE: &str = "table";
