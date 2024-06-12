8c8a53f9dcfbec589c3b22276d1a0dac55d2ed1b
0-don <don.cryptus@gmail.com>
2024-06-12 23:04:47 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
-    std::thread::spawn(|| scanner::main());
-    let _ = std::thread::spawn(|| api::main()).join();
+    scanner::main()
+
+    // std::thread::spawn(|| scanner::main());
+    // let _ = std::thread::spawn(|| api::main()).join();
 }
